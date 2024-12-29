3cb1e3ab12bfb6eb4e10007a39fc2d1268706a2c
SirDoggyJvla <106160594+SirDoggyJvla@users.noreply.github.com>
2024-12-29 17:45:08 +0100

    1..4.2
    Small fix for hotkeys not saving

--- Contents/mods/Check Upstairs/42/media/lua/client/CheckAround_ModOptions.lua
+++ Contents/mods/Check Upstairs/42/media/lua/client/CheckAround_ModOptions.lua
@@ -20,7 +20,7 @@ local options = PZAPI.ModOptions:create("CheckAround", "Check Around")
 options:addTitle("Check Around")
 options:addDescription("Check Around your surrondings for threats (windows, doors, stairs...)")
 options:addSeparator()
-local OPTION_CHECKUPSTAIRS = options:addKeyBind(0, "Check Upstairs", Keyboard.KEY_NONE)
+local OPTION_CHECKUPSTAIRS = options:addKeyBind("Check_Around", "Check Upstairs", Keyboard.KEY_NONE)
 
 return {
     CheckUpstairs = OPTION_CHECKUPSTAIRS,
